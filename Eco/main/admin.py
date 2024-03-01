aea91a47990b4a235d6bf1d58c0775b7828162a3
JosipCanadija <josip.canadija@gmail.com>
2024-03-01 17:48:39 +0100

    Updated models

--- Eco/main/admin.py
+++ Eco/main/admin.py
@@ -3,5 +3,5 @@ from .models import *
 
 # Register your models here.
 
-model_list = [UserProfile, UserEcoAction, EcoAction]
+model_list = [User, Organizer, EcoActivity, GroupAssistant]
 admin.site.register(model_list)
--- Eco/main/models.py
+++ Eco/main/models.py
@@ -1,27 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
-    location = models.CharField(max_length=300, blank=True)
-    age = models.IntegerField()
-
-    def __str__(self):
-        return self.user.username
-
-class EcoAction(models.Model):
-    name = models.CharField(max_length=300)
-    description = models.TextField()
-    points = models.BooleanField()
+class Organizer(models.Model):
+    org_name = models.CharField(max_length=20)
+    org_lastname = models.CharField(max_length=20)
+    org_contact = models.EmailField()
+    org_companyID = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.name
+        return self.org_companyID
 
-class UserEcoAction(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    eco_action = models.ForeignKey(EcoAction, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now_add=True)
+class EcoActivity(models.Model):
+    eco_name = models.CharField(max_length=50)
+    eco_summary = models.TextField()
+    eco_date = models.DateTimeField(default=timezone.now)
+    eco_author = models.ForeignKey(Organizer, default=1, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.user.username} - {self.eco_action.name}"
+        return self.eco_name
+
+class User(models.Model):
+    user_name = models.CharField(max_length=20)
+    user_lastname = models.Charfield(max_length=20)
+    user_id = models.CharField(max_length=8)
+    user_acitiv = models.ManyToManyField(EcoActivity)
+
+    def __str__(self):
+        return self.user_id
+
+class GroupAssistant(models.model):
+    mentor = models.OneToOneField(Organizer, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    ass_task = models.CharField(max_length=30)
+    ass_done = models.BooleanField(Default=False)
+
+    def __str__(self):
+        return 'Asistentski podaci usera sa ID-em {}'.format(self.user.user_id)
